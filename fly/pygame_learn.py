39e340f0c0350cd1672f5e9dec2e195fc1161d7b
littlediv <1299774687@qq.com>
2018-11-11 20:44:11 +0800

    game

--- fly/pygame_learn.py
+++ fly/pygame_learn.py
@@ -1,3 +1,5 @@
+# url https://www.cnblogs.com/wuzhanpeng/p/4261015.html#3767420
+
 import pygame                   # 导入 pygame 库
 from pygame.locals import *     # 导入 pygame 库中的一些常量
 from sys import exit            # 导入 sys 库中的exit 函数
@@ -13,8 +15,16 @@ screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 # 设置窗口标题
 pygame.display.set_caption('this is my first pygame-program')
 
+# 载入背景图
+background = pygame.image.load('resources/image/background.png')
 # 事件循环 (main loop)
 while True:
+
+    # 绘制背景
+    screen.blit(background, (0,0))
+    # 更新屏幕
+    pygame.display.update()
+
     # 处理游戏推出
     # 从消息队列中循环取
     for event in pygame.event.get():
